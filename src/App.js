9753bae2b8520f05c04b2b12d534aaaf006092eb
manishankar8096 <manishankar9988@gmail.com>
2022-06-12 16:54:38 +0530

    Implemented Routes and created basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+
+import Header from './components/Header';
 import './App.css';
+import Home from './Pages/Home';
+import Coin from './Pages/Coin';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>CryptoVerse</h1>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path='/coins/:coinId' element={<Coin/>}/>
+      </Routes>
+    </Router>
   );
 }
 
